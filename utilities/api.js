142501a84a39f8e2c9664aacdad68075ae1bbb89
harshithva <harshith11032001@gmail.com>
2022-04-20 14:47:47 +0530

    done

--- utilities/api.js
+++ utilities/api.js
@@ -1,4 +1,4 @@
-const url = 'https://admin.yamunaashacity.com'
+const url = 'https://admin.someshwarvista.com'
 // const url = 'http://127.0.0.1:8000'
 
 export const getHomepageSettings = async()=>{
