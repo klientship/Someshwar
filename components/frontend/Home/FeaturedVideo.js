fd6a7b5b03f38772496c930e62b2b53672c5f49a
harshithva <harshith11032001@gmail.com>
2022-04-20 15:46:33 +0530

    done

--- components/frontend/Home/FeaturedVideo.js
+++ components/frontend/Home/FeaturedVideo.js
@@ -4,7 +4,7 @@ const FeaturedVideo = () => {
     <div style={{width:"90%", margin:"auto", textAlign:"center", borderRadius:"50px", marginTop:"1em"}}>
         <iframe 
             className='embeded-youtube-video'
-            src="https://www.youtube.com/embed/DU_0tvCMM8E?autoplay=1&mute=1&controls=0" 
+            src="https://www.youtube.com/embed/8IOevdLslhc?autoplay=1&mute=1&controls=0" 
             title="YAMUNA ASHA CITY" 
             frameBorder="0" 
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
