73b9539a51953cfa5d265e880d160e66e648c273
harshithva <harshith11032001@gmail.com>
2022-05-23 12:14:59 +0530

    done

--- components/frontend/Home/FeaturedVideo.js
+++ components/frontend/Home/FeaturedVideo.js
@@ -1,7 +1,8 @@
 import React from 'react'
 const FeaturedVideo = (props) => {
   return (
-    <div style={{width:"90%", margin:"auto", textAlign:"center", borderRadius:"50px", marginTop:"1em"}}>
+  <div>
+     <div style={{width:"90%", margin:"auto", textAlign:"center", borderRadius:"50px", marginTop:"1em"}}>
         <iframe 
             className='embeded-youtube-video'
             src={props.settings.youtube_link} 
@@ -11,6 +12,28 @@ const FeaturedVideo = (props) => {
             allowFullScreen>
         </iframe>
     </div>
+    <div style={{width:"90%", margin:"auto", textAlign:"center", borderRadius:"50px", marginTop:"1em"}}>
+        <iframe 
+            className='embeded-youtube-video'
+            src={props.settings.youtube_link_2} 
+            title="YAMUNA ASHA CITY" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen>
+        </iframe>
+    </div>
+    <div style={{width:"90%", margin:"auto", textAlign:"center", borderRadius:"50px", marginTop:"1em"}}>
+        <iframe 
+            className='embeded-youtube-video'
+            src={props.settings.youtube_link_3} 
+            title="YAMUNA ASHA CITY" 
+            frameBorder="0" 
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" 
+            allowFullScreen>
+        </iframe>
+    </div>
+  </div>
+   
   )
 }
 
